b40136b7fffcd13cdb12aa5921b82302643cd829
Hasan Naseem <33922244+hnaseem1@users.noreply.github.com>
2018-02-05 14:46:06 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,8 +1,11 @@
+#essential libraries
 import json
 from difflib import get_close_matches
 
+#loads JSON data
 data = json.load(open("data.json"))
 
+#Function to find word in the data with cetain rules
 def translate(w):
 
     w = w.lower()
@@ -12,9 +15,13 @@ def translate(w):
         return data[w.title()]
     elif w.upper() in data:
         return data[w.upper()]
+ 
+# match the user input with the data for similar words (get_close_matches returns a list using similarity ratios in descending order)
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
+            
+#returns the first word from the suggestions (the first one is the most similar one)
             return data[get_close_matches(w, data.keys())[0]]
         elif yn == "N":
             return "The word doesn't exist, please double check.."
@@ -23,6 +30,7 @@ def translate(w):
     else:
         return "The word doesn't exist, please double check.."
 
+#Initialize 
 word = input("Enter word: ")
 
 output = translate(word)
